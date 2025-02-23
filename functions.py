092bdd991a5ff92bd2006546d9b70daa165fe30a
lschoch@gmail.com <lschoch@gmail.com>
2025-02-23 07:58:09 -0500

    functions.py: changed notifications popup interval to 4 hours.

--- functions.py
+++ functions.py
@@ -579,7 +579,7 @@ def notifications_popup(self):
                 else:
                     notifications_win.txt.insert("end", msg + "\n")
                     line_num += 1
-    self.after(60000, notifications_popup, self)
+    self.after(14400000, notifications_popup, self)
 
 
 # end notifications popup for upcoming events
