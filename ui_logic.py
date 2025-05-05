aaf617a34b819a3393d6e8843648b2374acec9db
lschoch@gmail.com <lschoch@gmail.com>
2025-05-05 11:49:14 -0400

    Submit fxn and create_preferences_fxn are now in their corresponding modules. Worked around circular import problem with deferred imports.

--- ui_logic.py
+++ ui_logic.py
@@ -1,17 +1,21 @@
 import tkinter as tk
+from datetime import date, datetime
 from tkinter import Menu, ttk
 
 from business_logic import (
     backup,
     create_new,
     delete_all,
+    get_user_data,
     opt_in,
     opt_out,
     preferences,
     restore,
+    save_prefs,
     view_all,
     view_pending,
 )
+from classes import InfoMsgBox
 
 
 def create_menu_bar(self):
@@ -114,3 +118,144 @@ def create_left_side_buttons(self):
     self.btn_new.grid(row=1, column=0, padx=20, pady=(20, 0), sticky="n")
     self.btn_quit = ttk.Button(self, text="Quit", command=self.destroy)
     self.btn_quit.grid(row=1, column=0, padx=20, pady=(60, 0), sticky="n")
+
+
+def create_preferences_window(self):
+    """
+    Create window for users to input/modify their notification preferences.
+    """
+
+    def submit():
+        week_before = self.var1.get()
+        day_before = self.var2.get()
+        day_of = self.var3.get()
+        none_selected = not any([week_before, day_before, day_of])
+        num = entry.get()
+        # validate the entered phone number
+        if not num.isnumeric() or len(num) > 10 or len(num) < 10:
+            InfoMsgBox(
+                self,
+                "Notifications",
+                "Phone number must be a ten digit numeric.",
+                x_offset=100,
+                y_offset=15,
+            )
+            entry.focus_set()
+            return
+        # require at least one "when" option
+        elif none_selected:
+            txt = (
+                "Please select at least one option for when "
+                + "to be notified."
+            )
+            InfoMsgBox(
+                self,
+                "Notifications",
+                txt,
+                x_offset=100,
+                y_offset=15,
+            )
+            preferences_window.focus_set()
+            return
+        else:
+            values = (
+                num,  # phone number
+                week_before,  # week before
+                day_before,  # day before
+                day_of,  # day of
+                # last notification date:
+                datetime.strftime(date.today(), "%Y-%m-%d"),
+            )
+            save_prefs(self, values)
+            preferences_window.destroy()
+
+    preferences_window = tk.Toplevel(self)
+    preferences_window.title("Notifications")
+    preferences_window.configure(background="#ececec")  # "#ffc49c")
+    preferences_window.geometry("300x185+100+50")
+    preferences_window.resizable(False, False)
+    preferences_window.wm_transient(self)
+    preferences_window.wait_visibility()
+    preferences_window.grab_set()
+    preferences_window.grid_columnconfigure(0, weight=1)
+    preferences_window.grid_columnconfigure(1, weight=1)
+
+    # create widgets for the window
+    ttk.Label(
+        preferences_window,
+        text="Enter your ten digit phone number:",
+        anchor="center",
+        background="#ececec",
+        font=("Helvetica", 13),
+    ).grid(row=0, column=0, columnspan=2, pady=(15, 7))
+    entry = ttk.Entry(preferences_window, font=("Helvetica", 13), width=10)
+    entry.grid(row=1, column=0, columnspan=2)
+
+    self.var1 = tk.IntVar()
+    self.var2 = tk.IntVar()
+    self.var3 = tk.IntVar()
+
+    ttk.Label(
+        preferences_window,
+        text="Notify when? Select all that apply:",
+        anchor="center",
+        background="#ececec",
+        font=("Helvetica", 13),
+    ).grid(row=2, column=0, columnspan=2, pady=(18, 2))
+
+    c1 = tk.Checkbutton(
+        preferences_window,
+        text="Week before",
+        font=("Helvetica", 12),
+        variable=self.var1,
+        onvalue=1,
+        offvalue=0,
+        background="#ececec",
+    )
+    c1.grid(row=3, column=0, columnspan=2, padx=(20, 0), sticky="w")
+    c2 = tk.Checkbutton(
+        preferences_window,
+        text="Day before",
+        font=("Helvetica", 12),
+        variable=self.var2,
+        onvalue=1,
+        offvalue=0,
+        background="#ececec",  # "#ececec",
+    )
+    c2.grid(row=3, column=0, columnspan=2, padx=(25, 0))
+
+    c3 = tk.Checkbutton(
+        preferences_window,
+        text="Day of",
+        font=("Helvetica", 12),
+        variable=self.var3,
+        onvalue=1,
+        offvalue=0,
+        background="#ececec",  # "#ececec",
+    )
+    c3.grid(row=3, column=0, columnspan=2, padx=(0, 25), sticky="e")
+
+    ttk.Button(
+        preferences_window,
+        text="Submit",
+        width=6,
+        command=submit,
+    ).grid(row=4, column=0, padx=(0, 15), pady=15, sticky="e")
+    ttk.Button(
+        preferences_window,
+        text="Cancel",
+        width=6,
+        command=preferences_window.destroy,
+    ).grid(row=4, column=1, padx=(15, 0), pady=15, sticky="w")
+
+    # Get existing preferences, if present, and insert into preferences window.
+    user_data = get_user_data(self)
+    if user_data[
+        0
+    ]:  # user_data[0] = phone number. Indicates that user exists.
+        entry.insert(0, user_data[0])
+        self.var1.set(user_data[1])  # week_before
+        self.var2.set(user_data[2])  # day_before
+        self.var3.set(user_data[3])  # day_of
+
+    entry.focus_set()
