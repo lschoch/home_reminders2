13696ad978a2aec94235132354495f8eeb28de42
lschoch@gmail.com <lschoch@gmail.com>
2025-05-21 13:48:40 -0400

    test_insert_data.py: Now mocking the app with tk.Tk() instead of the App class to avoid iconphoto issues. Expected values are now retrieved from the database instead of hardcoded.

--- tests/test_insert_data.py
+++ tests/test_insert_data.py
@@ -1,43 +1,61 @@
+import sqlite3
+import tkinter as tk
+
 from business import insert_data
-from home_reminders import App
+from ui_logic import create_tree_widget
 
 
 def test_insert_data(mocker, get_cursor):
     """
     Test the insert_data function.
     """
-    # Mock the treeview widget
-    mocker = App()
-    # Get the initial number of items in the treeview.
-    start_length = len(mocker.tree.get_children())
+    # Mock the treeview widget and insert data from the test database.
+    mocker = tk.Tk()
+    mocker.tree = create_tree_widget(mocker)
     insert_data(mocker, get_cursor)
+
     # Print the items in the treeview.
     for i in range(len(mocker.tree.get_children())):
         print(
             f"mocker.tree.get_children()[{i}]:\
                   {mocker.tree.item(mocker.tree.get_children()[i])['values']}"
         )
+
+    # Get data from the database to check against results of the insert_data
+    # function: a count of the number of items in the database and the
+    # description field of the first item in the database.
+    try:
+        with sqlite3.connect("./tests/test.db") as con:
+            cur = con.cursor()
+            cur.execute("SELECT COUNT(*) FROM reminders")
+            db_count = cur.fetchone()[0]
+            print(f"db_count: {db_count}")
+            # Select the first item in the database.
+            cur.execute("SELECT * FROM reminders LIMIT 1")
+            # Get the description field of the first item in the database.
+            description = cur.fetchone()[1]
+    except sqlite3.Error as e:
+        print(f"Error retrieving data from test database: {e}")
+        db_count = 0
+
     # Add assertions to verify the expected behavior
 
-    # Check if the number of items in the treeview has increased by 2, the
-    # of items in the test database.
-    assert len(mocker.tree.get_children()) == start_length + 2
+    # Check if all items in the database have been inserted into the treeview.
+    assert len(mocker.tree.get_children()) == db_count
 
     # Check if the first item inserted in the treeview has the expected text
     assert (
-        mocker.tree.item(mocker.tree.get_children()[start_length])["values"][1]
-        == "testing insert data function"
+        mocker.tree.item(mocker.tree.get_children()[0])["values"][1]
+        == description
     )
 
     # Check if the first item inserted in the test database has the expected
-    # background color (highlighted).
-    first_inserted_item = mocker.tree.item(
-        mocker.tree.get_children()[start_length]
-    )
-    # tag_name is the database index of the item
+    # background color (highlighting).
+    first_inserted_item = mocker.tree.item(mocker.tree.get_children()[0])
+    # Item tags are named as the database index of the item.
     tag_name = first_inserted_item["values"][0]
     print(f"tag_name: {tag_name}")
-    # Query the tag configuration for the background color.
+    # Query the tag configuration to get the background color.
     print(f"tag color: {mocker.tree.tag_configure(tag_name, 'background')}")
     # Check if the tag has the expected background color (yellow).
     assert str(mocker.tree.tag_configure(tag_name, "background")) == "yellow"
