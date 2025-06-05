9ea2e4fe16cb9843f7991857eaa84ab7ebd8a7e6
lschoch@gmail.com <lschoch@gmail.com>
2025-06-04 21:47:34 -0400

    test_save_prefs.py: test is skipped if not in test environment, now backing up an restoring the test database, functions to cleanup, modified error handling to ensure that test.db is restored.

--- tests/test_save_prefs.py
+++ tests/test_save_prefs.py
@@ -1,69 +1,57 @@
+import os
+import shutil
 import tkinter as tk
 
 import pytest
 from loguru import logger
 
 from business import get_con, save_prefs
+from constants import DB_ENVIRONMENT
 
 
 def test_save_prefs():
+    # Skip this test if not in tesst environment.
+    if DB_ENVIRONMENT != "test":
+        pytest.skip("Skipping this test - not in test environment.")
+
+    # Make a temporary copy of the test database so that it can be restored
+    # later.
+    db_path = os.path.join(os.path.dirname(__file__), "test.db")
+    db_bak_path = os.path.join(os.path.dirname(__file__), "test_bak.db")
+    shutil.copy2(db_path, db_bak_path)
+
+    def cleanup():
+        # Restore the test database.
+        shutil.copy2(db_bak_path, db_path)
+        # Delete the temporary copy of the test database.
+        os.remove(db_bak_path)
+        app.destroy()
+
+    def error_cleanup(e, msg):
+        logger.error(msg + f": {e}," + " skipping this test.")
+        cleanup()
+        pytest.skip(msg + ".")
+
     # Mock the tkinter app and set the preference values for the test.
     app = tk.Tk()
     values = ("1234567890", 1, 1, 1, "1970-01-01")
 
-    # Save the existing preferences so they can be restored later.
-    try:
-        with get_con() as con:
-            cur = con.cursor()
-            cur.execute("SELECT * FROM user")
-            original_values = cur.fetchone()
-    # Skip test if unable to retrieve original values.
-    except Exception as e:
-        # Log the error and skip the test.
-        logger.error(
-            f"An error occurred while retrieving original values: {e}, "
-            "skipping this test."
-        )
-        app.destroy()
-        pytest.skip(
-            "An error occurred while retrieving original values. Skipping"
-            " this test."
-        )
     # Use the save_prefs function to save the test preferences to the database.
     # Test if the values have been successfully saved.
-    save_prefs(app, values)
+    try:
+        save_prefs(app, values)
+    except Exception as e:
+        error_cleanup(e, "An error occurred calling the save_prefs function")
+    # Retrieve the results of the function.
     try:
         with get_con() as con:
             cur = con.cursor()
             cur.execute("SELECT * FROM user")
             new_values = cur.fetchone()
-            assert new_values == values
     except Exception as e:
-        logger.error(f"An error occurred while checking the saved values: {e}")
-    finally:
-        # Restore the original preferences after the test.
-        try:
-            with get_con() as con:
-                cur = con.cursor()
-                cur.execute("DELETE FROM user")
-                cur.execute(
-                    """INSERT INTO user (
-                    phone_number,
-                    week_before,
-                    day_before,
-                    day_of,
-                    last_notification_date) VALUES(?, ?, ?, ?, ?)""",
-                    original_values,
-                )
-                con.commit()
-                # Check if the values have been restored.
-                cur.execute("SELECT * FROM user")
-                restored_values = cur.fetchone()
-                assert restored_values == original_values
-        except Exception as e:
-            logger.error(
-                f"An error occurred while restoring the original values: {e}. "
-                "User table may be corrupted."
-            )
-        finally:
-            app.destroy()
+        error_cleanup(e, "Error while retrieving the saved values")
+    # If no error, cleanup before checking the results - so that cleanup occurs
+    # even if there are assertion errors.
+    cleanup()
+    # Check results.
+    assert new_values == values
